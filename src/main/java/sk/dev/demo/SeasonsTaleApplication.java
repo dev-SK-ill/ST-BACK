3ce118856612f95a4b9c50a394e2c459eeed7e7b
김경준 <kkj990@jkds.kr>
2024-06-12 15:05:24 +0900

    first commit

---
+++ src/main/java/sk/dev/demo/SeasonsTaleApplication.java
@@ -0,0 +1,13 @@
+package sk.dev.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SeasonsTaleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SeasonsTaleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/sk/dev/demo/SeasonsTaleApplicationTests.java
@@ -0,0 +1,13 @@
+package sk.dev.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SeasonsTaleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
